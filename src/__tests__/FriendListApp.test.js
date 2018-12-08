7441f38a13c0f7b9a49825680707c86dbaf77024
Shishir Arora <sarora0@walmartlabs.com>
2018-12-09 01:38:02 +0530

    wip

--- src/__tests__/FriendListApp.test.js
+++ src/__tests__/FriendListApp.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 import {FriendListApp} from '../containers/FriendListApp';
 import toJson from 'enzyme-to-json';
 
@@ -21,14 +20,58 @@ const create = () => {
   return { store, next, invoke };
 };
 
-/*it('passes through non-function action', () => {
-  const { next, invoke } = create();
-  const action = { type: 'TEST' };
-  invoke(action);
-  expect(next).toHaveBeenCalledWith(action)
-});*/
-it('renders the component', () => {
-  const tree = renderer.create(<FriendListApp />).toJSON();
-  expect(tree).toMatchSnapshot();
+function getProps() {
+  const friendlist = {
+    friendsById: [
+      {
+        name: 'Theodore Roosevelt',
+        starred: true
+      },
+      {
+        name: 'Abraham Lincoln',
+        starred: false
+      },
+      {
+        name: 'George Washington',
+        starred: false
+      }
+    ]
+  };
+  const addFriend = jest.fn();
+  const deleteFriend = jest.fn();
+  const starFriend = jest.fn();
+  const updateSex = jest.fn();
+  const nextPage = jest.fn();
+  const prevPage = jest.fn();
+  const page = {pageNo: 1};
+  const props = {
+    ...{
+      friendlist, addFriend, deleteFriend, starFriend, updateSex,
+      nextPage, prevPage,
+      page
+    }
+  };
+  return {prevPage, props, nextPage};
+}
+
+describe('test FriendListApp', ()=>{
+  const {prevPage, nextPage, props} = getProps();
+  const component = mount(
+    <FriendListApp
+      {...props}
+    />
+  );
+  it('renders the component', () => {
+    expect(toJson(component)).toMatchSnapshot();
+  });
+  it('prev button click calls prevPage action', () => {
+    component.find('Pagination').find('button').at(0).simulate('click');
+    expect(prevPage).toHaveBeenCalled();
+  });
+  it('next button click  nextPage action', () => {
+    component.find('Pagination').find('button').at(1).simulate('click');
+    expect(nextPage).toHaveBeenCalled();
+  });
 });
+
 
